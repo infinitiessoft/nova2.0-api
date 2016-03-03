8cdecb633d7ec7e886eba79b99496cab5dcacd14
Po-Hsun <pohsun@infinitiesoft.com>
2016-03-03 16:46:34 +0800

    remove neutron-related code

--- src/main/java/com/infinities/neutron/ports/model/PortForCreate.java
+++
@@ -1,8 +0,0 @@
-package com.infinities.neutron.ports.model;
-
-import org.codehaus.jackson.map.annotate.JsonRootName;
-
-@SuppressWarnings("serial")
-@JsonRootName("port")
-public class PortForCreate extends Port {
-}
